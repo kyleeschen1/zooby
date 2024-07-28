0b3aebdb3e29eee3b99dd1a37eb0da1b70f86592
Kyle Eschen <kyleeschen@MacBook-Pro.local>
2024-07-28 11:04:35 -0700

    Added environment object.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import subprocess
 import curses
 
 
-# output = subprocess.run(["echo", "Geeks for geeks"], capture_output=True)
+# output = Subprocess.run(["echo", "Geeks for geeks"], capture_output=True)
 # print("Hello" + output.stdout.decode("utf-8"))
 
 
@@ -24,12 +24,25 @@ def apply_cmd(state, cmd):
         cmd(state)
 
 
+class Env:
+
+    def __init__(self):
+        self.locals = {}
+
+    def bind(self, key, value):
+        self.locals[key] = value
+
+    def resolve(self, key):
+        return self.locals[key]
+
+
 class State:
 
     def __init__(self, screen, cmd_map):
         self.screen = screen
         self.cmd_map = cmd_map
         self.continue_running = True
+        self.env = Env()
 
     def run(self):
         cmd = dispatch_on_key(self.key, self.cmd_map)
@@ -52,23 +65,37 @@ def list_fn(state, args):
     print(args)
 
 
+def shell(state, shell_cmd):
+    output = subprocess.run(shell_cmd, capture_output=True)
+    output = output.stdout.decode("utf-8")
+    state.output = output
+    print(output)
+
+
 cmd_map_init = {
     "default": display_output,
     "q": quit_loop,
     "c": clear,
     "d": [list_fn, 1, 2, 3],
+    "s": [shell, "pwd"],
 }
+
+
+def run_event_loop(state):
+    while state.continue_running:
+        state.key = state.screen.getkey()
+        state.run()
+        state.screen.refresh()
 
 
 def main(screen):
 
+    # Initialize state and screen
     screen.clear()
     state = State(screen, cmd_map_init)
 
-    while state.continue_running:
-        state.key = screen.getkey()
-        state.run()
-        screen.refresh()
+    # Run the event loop
+    run_event_loop(state)
 
 
 if __name__ == "__main__":
